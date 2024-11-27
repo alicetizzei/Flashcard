73e49c569669babfecc0085ca5b12a632e0221bf
alice tizzei <alicetizzei@gmail.com>
2024-11-27 15:35:30 -0300

    update

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'ACADEMIA',
+    'Quem tem problema na coluna pode treinar?',
+    'em geral, a musculação pode ser uma atividade segura e benéfica para quem tem problemas na coluna, desde que seja feita de forma adequada e com acompanhamento de um profissional qualificado.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Mulher que faz treino de peito faz ele diminuir?',
+    'Não, Esquecem-se que uma mulher tem um metabolismo, organismo, sistema hormonal que é completamente diferente dos homens, logo mesmo a treinarem da mesma forma nunca obteriam os mesmos resultados.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Posso treinar todos os dias?',
+    'Não é recomendável treinar todos os dias depois isso pode sobrecarregar os músculos causando estresse muscular e probllemas nas articulações.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Crianças podem treinar?',
+    'Não há uma idade universal para crianças frequentarem academias, mas é necessário que sejam supervisionadas.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Tenho problema cardíaco, posso treinar?',
+    'Depende do estado da pessoa, se for um estado grave resultaria em outros problemas, porem se for um estado mais leve não agravaria em nada.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Academia auxilia na diminuição de extresse e ansiedade?',
+    'Sim, com toda certeza, podendo tambem contribuir para o aumento de autoestima.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Posso treinar todos os dias?',
+    'Não é recomendado treinar todos os dias, pois pode aumentar o estresse muscular e lesões.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Preciso comer antes de treinar?',
+    'É recomendado ingerir alimentos que auxiliam na hora do esforço.'
+)
+
+criaCartao(
+    'ACADEMIA',
+    'Para que serve a creatina?',
+    '>A creatina serve como fonte de produção de energia para as células musculares, de modo que melhora a força e o tônus muscular.'
+)
